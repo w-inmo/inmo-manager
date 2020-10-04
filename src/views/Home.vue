005ce64bbd0b5870813b4a09a78ed43366981240
inmo <inmo@inmodeMacBook-Pro.local>
2020-10-04 13:35:16 +0800

    修改 avatar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,8 +29,7 @@
         name: 'Home',
         data() {
             return {
-                avatarUrl: '../assets/images/avatar.png',
-                url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+                url: 'http://cdn.eqingchen.top/blog/icon.png'
             }
         },
         components: {
